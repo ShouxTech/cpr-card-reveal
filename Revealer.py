726dfabb7485dc21d43ce581a8ae96596e9dee6c
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2020-11-28 19:13:01 -0500

    Update Revealer.py

--- Revealer.py
+++ Revealer.py
@@ -15,8 +15,6 @@ class Interface:
             add_spacing(count = 2)
             add_drawing('Card', width = 400, height = 400)
 
-        draw_image('Card', './cards/1.png', pmin = [0, 0], pmax = [250, 280], tag = 'image')
-
         Thread(target = start_dearpygui).start()
 
     def connected(self):
@@ -29,16 +27,17 @@ class ReceivePacket(events.EventEmitter):
     def __init__(self, interface):
         super().__init__()
         self.interface = interface
+        self.target_packets = ['%jr%', '%jz%', '%zm%']
         self.connected = False
 
     def start(self):
         def receive_packet(packet):
             packet_data = str(packet.load)
-            tcp_packet_data_start = packet_data.find('%xt')
-            if tcp_packet_data_start != -1 and (not self.connected):
+            is_xt_packet = '%xt' in packet_data
+            if is_xt_packet and (not self.connected):
                 self.connected = True
                 self.interface.connected()
-            if (packet_data.find('%jr%') != -1) or (packet_data.find('%jz%') != -1) or (packet_data.find('%zm%') != -1):
+            if any(substring in packet_data for substring in self.target_packets):
                 self.emit('packet', packet_data)
 
         sniff(filter = 'tcp and src 85.217.222.71', prn = receive_packet)
@@ -59,7 +58,7 @@ class Revealer():
     def get_cards(self, packet):
         deconstructed = packet.split('|')
 
-        self.cards = { # ID of first card, second card, etc...
+        self.cards = { # ID of first card, second card, etc
             0: deconstructed[1],
             1: deconstructed[6],
             2: deconstructed[11],
@@ -82,7 +81,7 @@ class Revealer():
 
     def handle_zm(self, packet, deconstructed_packet):
         action = deconstructed_packet[4] # Deal/Judge/Pick
-        side = deconstructed_packet[5] # The penguin's side that the packet came from
+        side = deconstructed_packet[5] # The penguins side that the packet came from
         if (side == self.side) or (action == 'judge'):
             return
 
